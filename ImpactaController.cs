a449895e2259df855dfd84e61a1e508b8c74f645
Rhayssa Kramer <112133224+rhayssakramer@users.noreply.github.com>
2024-04-29 12:09:41 -0300

    feats: implementação no AspNet

---
+++ ImpactaController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace cursoImpacta.Controllers
+{
+    public class ImpactaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Aluno()
+        {
+            return View();
+        }
+
+        public IActionResult Curso()
+        {
+            return View();
+        }
+    }
+}
