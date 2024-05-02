6f5d2cca7e8afc0c01a6c1febe2582d604c8ed9d
Rhayssa Kramer <112133224+rhayssakramer@users.noreply.github.com>
2024-05-02 09:23:58 -0300

    feats: implementação do AspNet e organização do repositório

---
+++ VisualStudio/Controllers/ImpactaController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace cursoImpacta.Models
+{
+    public class ImpactaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Aluno()
+        {
+            return View();
+        }
+
+        public IActionResult Curso => View();
+    }
+}
